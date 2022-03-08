7b2dff438c047eba511062c399090a365ca702a9
Douglas Yarrington <82406499+yarri-oss@users.noreply.github.com>
2022-03-07 16:24:38 -0800

    Update LandmarkPlanckComponent.js

--- components/LandmarkPlanckComponent.js
+++ components/LandmarkPlanckComponent.js
@@ -112,7 +112,7 @@ const PixiComponent = () => {
             <Graphics draw={drawEdges} />
             <DynamicCoin speed={0.01} />
             <Text text="Click anywhere..." anchor={0.5} x={110} y={20} />
-            <Text text="Or Move Your Hands!" anchor={0.5} x={110} y={40} />
+            <Text text="Or Move Your Hands!" anchor={0.5} x={130} y={45} />
         </Stage>
     )
 }
